0793b31bdfed00a5fa8b053d332806d88e75d1eb
Ironhack <ironhack@MacBook-Air-de-Ironhack.local>
2024-05-03 16:05:38 +0200

    lab complete

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,30 @@ import ExponentThree from "./components/ExponentThree";
 import ExponentFour from "./components/ExponentFour";
 import ExponentFive from "./components/ExponentFive";
 import ExponentSix from "./components/ExponentSix";
+import { useState } from "react";
 
 
 function App () {
+  const [count, setCount] = useState(0);
+
+  const exponent = 3
   return (
+
+    
     <div className="App">
       <h2><em>Counter</em></h2>
-  
-      <Counter/>
+      <p className="counter-value">{count}</p>
+      <Counter count = {count} setCount = {setCount}/>
 
       <br />
       <h2><em>Exponents</em></h2>
 
       <div className="container">
-        <ExponentTwo />
-        <ExponentThree />
-        <ExponentFour />
-        <ExponentFive />
-        <ExponentSix />
+        <ExponentTwo  count = {count} setCount = {setCount}/>
+        <ExponentThree count = {count} setCount = {setCount}/>
+        <ExponentFour count = {count} setCount = {setCount}/>
+        <ExponentFive count = {count} setCount = {setCount}/>
+        <ExponentSix count = {count} setCount = {setCount}/>
       </div>
     </div>
   );
